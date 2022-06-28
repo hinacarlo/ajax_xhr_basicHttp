6075b290efb1d020ac56e085d969ff868a7602f8
carlohina <hinacarlo1993@gmail.com>
2022-06-28 13:16:49 +0800

    Create basic files

---
+++ basicHttp.js
@@ -0,0 +1,10 @@
+class basicHTTP {
+  constructor() {
+    this.http = new XMLHttpRequest();
+  }
+}
+
+// Make an HTTP GET Request
+// Make an HTTP POST Request
+// Make an HTTP PUT Request
+// Make an HTTP DELETE Request
